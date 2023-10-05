d015bb49986979bce654efc82f9e1d1387b9fd44
JiyaYUN <142545110+JiyaYUN@users.noreply.github.com>
2023-10-05 00:05:25 -0400

    Update CoreStat_Assignment4_StockAnalysis.R

--- CoreStat_Assignment4_StockAnalysis.R
+++ CoreStat_Assignment4_StockAnalysis.R
@@ -29,5 +29,68 @@ AAPL_Stock <- read.csv(AAPL)
 CSCO_Stock <- read.csv(CSCO)
 HD_Stock <- read.csv(HD)
 VZ_Stock <- read.csv(VZ)
-W5000_Stock <- read.csv(W5000)
-Mkt_R_Rf <- read.csv(MktRf, header = TRUE, fill = TRUE)
+
+W5000_Stock <- read.csv("D:/Simon.UR/Fall A/GBA462 Core Statistics/Core Stat Assignment/^W5000.csv")
+MktRf <- read.csv("D:/Simon.UR/Fall A/GBA462 Core Statistics/Core Stat Assignment/F-F_Research_Data_Factors.CSV")
+
+library(dplyr)
+AAPL_Stock <- na.omit(
+     AAPL_Stock %>%
+          select(Date, Open) %>%
+          mutate(Date = as.Date(Date, format = "%m/%d/%Y"),
+                 R = (Open - lag(Open)) / lag(Open)
+          )
+)
+
+CSCO_Stock <- na.omit(
+     CSCO_Stock %>%
+          select(Date, Open) %>%
+          mutate(Date = as.Date(Date, format = "%m/%d/%Y"),
+                 R = (Open - lag(Open)) / lag(Open)
+          )
+)
+
+HD_Stock <- na.omit(
+     HD_Stock %>%
+          select(Date, Open) %>%
+          mutate(Date = as.Date(Date, format = "%m/%d/%Y"),
+                 R = (Open - lag(Open)) / lag(Open)
+          )
+)
+
+VZ_Stock <- na.omit(
+     VZ_Stock %>%
+          select(Date, Open) %>%
+          mutate(Date = as.Date(Date, format = "%m/%d/%Y"),
+                 R = (Open - lag(Open)) / lag(Open)
+          )
+)
+
+MktRf <- na.omit(
+     MktRf %>%
+     select(Date, MktRF, RF) %>%
+     mutate(
+          Date = as.Date(Date, format = "%m/%d/%Y"),
+          Rm = (MktRF - lag(MktRF)) / lag(MktRF),
+          Rf = (RF - lag(RF)) / lag(RF)
+     )
+)
+
+W5000_Stock <- na.omit(
+     W5000_Stock %>% 
+     select(Date, Open) %>%
+     mutate(
+          Date = as.Date(Date, format = "%m/%d/%Y"), 
+          Open = (Open) / 100,
+          R = Open - lag(Open) / lag(Open)
+     )
+)
+
+Mktdf <- merge(W5000_Stock, MktRf, by = "Date") %>%
+     mutate(market_excess_return = R - RF)
+
+AAPL_Stock= merge(AAPL_Stock, MktRf, by = "Date")
+AAPL_Stock = AAPL_Stock %>% mutate(excess_return = R - Rf)
+
+ols = lm(AAPL_Stock$excess_return ~ Mktdf$market_excess_return)
+print(summary(ols)) 
