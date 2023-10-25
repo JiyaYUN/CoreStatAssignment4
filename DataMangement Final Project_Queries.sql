b66561fa02c5849107275d0d44bbbeee1410e84b
JiyaYUN <142545110+JiyaYUN@users.noreply.github.com>
2023-10-25 01:12:42 -0400

    Update DataMangement Final Project_Queries.sql

--- DataMangement Final Project_Queries.sql
+++ DataMangement Final Project_Queries.sql
@@ -102,16 +102,14 @@ DROP TABLE IF EXISTS CustomerOrder;
 CREATE OR REPLACE VIEW CustomerOrder AS
 SELECT
     orderQuery.OrderID, c.CustomerID, c.ContactName AS CustomerName, c.Country AS CustomerCountry, c.City AS CustomerCity,
-    orderQuery.OrderDate, orderQuery.ShippedDate, orderQuery.ShipCountry, orderQuery.ShipCity, 
-    shipperquery.ShipperID, shipperquery.ShipperName, order_detailsQuery.Quantity,
-    order_detailsQuery.Discount, productQuery.ProductID, productQuery.ProductName,
-    productQuery.SupplierID, productQuery.CategoryID,productQuery.UnitPrice, productQuery.UnitsInStock,
+    orderQuery.OrderDate, orderQuery.ShippedDate, orderQuery.ShipCountry, orderQuery.ShipCity, orderQuery.Freight, shipperquery.ShipperID, shipperquery.ShipperName, 
+    order_detailsQuery.Quantity, order_detailsQuery.Discount, productQuery.ProductID, productQuery.ProductName,
+    productQuery.CategoryID, productQuery.UnitPrice, productQuery.UnitsInStock,
     productQuery.UnitsOnOrder,productQuery.ReorderLevel,productQuery.Discontinued,
-    supplierQuery.CompanyName AS SupplierName,supplierQuery.Country AS SupplyCountry,supplierQuery.City AS SupplyCity,
     categoryQuery.CategoryName
 FROM customers c
 JOIN (
-    SELECT OrderID, CustomerID, OrderDate, ShippedDate, ShipCity, ShipCountry, ShipVia
+    SELECT OrderID, CustomerID, OrderDate, ShippedDate, ShipCity, ShipCountry, ShipVia, Freight
     FROM orders
     GROUP BY OrderID
 ) AS orderQuery ON orderQuery.CustomerID = c.CustomerID
@@ -129,13 +127,71 @@ JOIN (
     FROM products
 ) AS productQuery ON productQuery.ProductID = order_detailsQuery.ProductID
 JOIN (
-    SELECT SupplierID, CompanyName, ContactName, Country, City
-    FROM suppliers
-) AS supplierQuery ON supplierQuery.SupplierID = productQuery.SupplierID
-JOIN (
     SELECT CategoryID, CategoryName
     FROM categories
 ) AS categoryQuery ON categoryQuery.CategoryID = productQuery.CategoryID;
 
 SELECT * FROM CustomerOrder;
+
+-- find the countries of top 10 sales
+SELECT CustomerCountry, ROUND(Sum((UnitPrice*Quantity*(1-Discount)/100)*100),2) AS Sales
+FROM customerorder
+GROUP BY CustomerCountry 
+ORDER BY Sales DESC LIMIT 10;
+
+-- find the number of orders that were shipped over a one-week period and group the results by three expressions
+SELECT ShipperID, ShipperName, COUNT(OrderID) AS NumberOfOrders
+FROM (
+    SELECT 
+        OrderID,
+        ShipperID,
+        ShipperName
+    FROM CustomerOrder
+    WHERE DATEDIFF(STR_TO_DATE(ShippedDate, '%c/%e/%Y'), STR_TO_DATE(OrderDate, '%c/%e/%Y')) > 7
+) AS OrderDateDifference
+GROUP BY ShipperID
+ORDER BY NumberOfOrders DESC;
+
+--  find the popular category with more orders
+SELECT CategoryName, COUNT(ProductName) AS NumberOfOrder
+FROM customerorder
+GROUP BY CategoryName
+ORDER BY NumberOfOrder DESC;
+
+-- find the number of orders in each category with a discount
+SELECT CategoryName, SUM(CASE WHEN Discount = 0 THEN 0 ELSE 1 END) AS CountOfOrdersWithDiscount
+FROM CustomerOrder
+GROUP BY CategoryName;
+
+-- find categories with a higher number of inventory and assign them to classes
+SELECT CategoryName, ProductID, ProductName, Inventory,
+NTILE(5) OVER (ORDER BY Inventory) AS InventoryClass
+FROM(
+SELECT CategoryName, ProductID, ProductName, SUM(UnitsInStock + UnitsOnOrder) AS Inventory
+FROM CustomerOrder
+GROUP BY CategoryName
+) AS InventoryQuery
+ORDER BY Inventory DESC;
+
+-- find customer whose order is closer to current date and more order value
+SELECT CustomerID, CustomerName, MAX(CURDATE() - STR_TO_DATE(ShippedDate, '%c/%e/%Y')) AS RecentOrderAge,
+MAX(UnitPrice * Quantity * (1 - Discount)) AS MaxOrderValue
+FROM customerorder
+GROUP BY CustomerID
+ORDER BY RecentOrderAge DESC, MaxOrderValue DESC;
+
+-- find the orders where the customer's city matches the shipper's city
+SELECT OrderID, ProductName, CategoryName, ROUND((UnitPrice * Quantity * (1 - Discount) - Freight), 2) AS TotalPayment
+FROM customerorder
+WHERE CustomerCity = ShipCity
+ORDER BY TotalPayment DESC;
+
+--  find the category with the top total payment
+SELECT CategoryName, ROUND(SUM((UnitPrice * Quantity * (1 - Discount) - Freight)),2) AS TotalPayment
+FROM customerorder
+GROUP BY CategoryName
+ORDER BY TotalPayment DESC;
+
+
+
 
